7b5c3bd6f4d37e803f20c4688c01ff0b77bf2fe8
Ferista <dfp.schoen@gmx.de>
2015-05-19 15:43:10 +0200

    Mousecolision test added

---
+++ Tests/Mouse/Grid/Grid/Hexagon.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Grid
+{
+    class Hexagon
+    {
+        VertexPositionColor[] vertices = new VertexPositionColor[6];
+        Vector3 position;
+        Color standartColor;
+        int indexNumber;
+
+        public Hexagon(Vector3 position, float sidelength, Color color, int indexNumber)
+        {
+            standartColor = color;
+            this.position = position;
+            this.indexNumber = indexNumber;
+            vertices[0] = new VertexPositionColor(position + new Vector3(-sidelength, 0.0f, 0.0f), color);
+            vertices[1] = new VertexPositionColor(position + new Vector3(-sidelength / 2, sidelength * 7 / 8, 0.0f), color);
+            vertices[2] = new VertexPositionColor(position + new Vector3(-sidelength / 2, -sidelength * 7 / 8, 0.0f), color);
+            vertices[3] = new VertexPositionColor(position + new Vector3(sidelength / 2, sidelength * 7 / 8, 0.0f), color);
+            vertices[4] = new VertexPositionColor(position + new Vector3(sidelength / 2, -sidelength * 7 / 8, 0.0f), color);
+            vertices[5] = new VertexPositionColor(position + new Vector3(sidelength, 0.0f, 0.0f), color);
+        }
+
+        public int getIndexNumber() { return indexNumber; }
+        public Vector2 get2DPosition() { return new Vector2(position.X, position.Z); }
+        public void setColor(Color color) 
+        {
+            for (int i = 0; i < 6; ++i)
+            {
+                vertices[i].Color = color;
+            }
+        }
+        public Color getStColor() { return standartColor; }
+
+        public void Draw(GameTime gameTime, GraphicsDevice graphics)
+        {
+            graphics.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.TriangleStrip, vertices, 0, 4);
+        }
+    }
+}
---
+++ Tests/Mouse/Grid/Grid/Plane.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Grid
+{
+    class Plane
+    {
+        List<Hexagon> plane = new List<Hexagon>();
+        int indexNumber = 0;
+        int sidelength;
+
+        public Plane(int sidelength, float hexagonsidelength)
+        {
+            this.sidelength = sidelength;
+            for (int i = 0; i < sidelength; ++i)
+            {
+                for (int j = 0; j < sidelength; ++j)
+                {
+                    /*plane.Add(new Hexagon(new Microsoft.Xna.Framework.Vector3(i * (hexagonsidelength + hexagonsidelength * (float)Math.Sqrt(2)) + hexagonsidelength / 2 + hexagonsidelength / (float)Math.Sqrt(2),
+                                                                              hexagonsidelength / (float)Math.Sqrt(2), 0), hexagonsidelength, Color.White));*/
+
+                    /*if (j % 2 == 0) plane.Add(new Hexagon(new Vector3(i * 3 * hexagonsidelength + hexagonsidelength,
+                                                                        j * hexagonsidelength * 7 / 8, 0), hexagonsidelength, Color.White));
+                    else plane.Add(new Hexagon(new Vector3(i * 3 * hexagonsidelength + 2.5f*hexagonsidelength,
+                                                           j * hexagonsidelength * 7 / 8, 0), hexagonsidelength, Color.White));*/
+
+                    if (i % 2 == 0)
+                    {
+                        if (i >= 5 && j >= 5)
+                        {
+                            plane.Add(new Hexagon(new Vector3(i * 3 / 2 * hexagonsidelength + hexagonsidelength,
+                                   j * 2 * hexagonsidelength * 7 / 8 + hexagonsidelength * 7 / 8, 0), hexagonsidelength, Color.Blue, indexNumber));
+                            ++indexNumber;
+                        }
+                        else if (i >= 5)
+                        {
+                            plane.Add(new Hexagon(new Vector3(i * 3 / 2 * hexagonsidelength + hexagonsidelength,
+                                   j * 2 * hexagonsidelength * 7 / 8 + hexagonsidelength * 7 / 8, 0), hexagonsidelength, Color.Green, indexNumber));
+                            ++indexNumber;
+                        }
+                        else if (j >= 5)
+                        {
+                            plane.Add(new Hexagon(new Vector3(i * 3 / 2 * hexagonsidelength + hexagonsidelength,
+                                   j * 2 * hexagonsidelength * 7 / 8 + hexagonsidelength * 7 / 8, 0), hexagonsidelength, Color.Red, indexNumber));
+                            ++indexNumber;
+                        }
+                        else
+                        {
+                            plane.Add(new Hexagon(new Vector3(i * 3 / 2 * hexagonsidelength + hexagonsidelength,
+                                   j * 2 * hexagonsidelength * 7 / 8 + hexagonsidelength * 7 / 8, 0), hexagonsidelength, Color.Yellow, indexNumber));
+                            ++indexNumber;
+                        }
+                    }
+                    else
+                    {
+                        if (i >= 5 && j >= 5)
+                        {
+                            plane.Add(new Hexagon(new Vector3((i - 1) * 3 / 2 * hexagonsidelength + 2.5f * hexagonsidelength,
+                                   j * 2 * hexagonsidelength * 7 / 8 + 2 * hexagonsidelength * 7 / 8, 0), hexagonsidelength, Color.Blue, indexNumber));
+                            ++indexNumber;
+                        }
+                        else if (i >= 5)
+                        {
+                            plane.Add(new Hexagon(new Vector3((i - 1) * 3 / 2 * hexagonsidelength + 2.5f * hexagonsidelength,
+                                   j * 2 * hexagonsidelength * 7 / 8 + 2 * hexagonsidelength * 7 / 8, 0), hexagonsidelength, Color.Green, indexNumber));
+                            ++indexNumber;
+                        }
+                        else if (j >= 5)
+                        {
+                            plane.Add(new Hexagon(new Vector3((i - 1) * 3 / 2 * hexagonsidelength + 2.5f * hexagonsidelength,
+                                   j * 2 * hexagonsidelength * 7 / 8 + 2 * hexagonsidelength * 7 / 8, 0), hexagonsidelength, Color.Red, indexNumber));
+                            ++indexNumber;
+                        }
+                        else
+                        {
+                            plane.Add(new Hexagon(new Vector3((i - 1) * 3 / 2 * hexagonsidelength + 2.5f * hexagonsidelength,
+                                   j * 2 * hexagonsidelength * 7 / 8 + 2 * hexagonsidelength * 7 / 8, 0), hexagonsidelength, Color.Yellow, indexNumber));
+                            ++indexNumber;
+                        }
+                    }
+     ;          }
+            }
+        }
+
+        public int getSideLength() { return sidelength; }
+
+        public List<Hexagon> getPlaneHexagons() { return plane; }
+
+        public void Draw(GameTime gameTime, GraphicsDevice graphics)
+        {
+            for (int i = 0; i < plane.Count; ++i)
+            {
+                plane[i].Draw(gameTime, graphics);
+            }
+        }
+    }
+}

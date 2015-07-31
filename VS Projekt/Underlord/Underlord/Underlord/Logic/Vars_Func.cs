c51d0dc2d32bd5cb7c0ae484c55668a534632f43
FNDominik <fnd.schoen@gmx.de>
2015-07-31 17:45:19 +0200

    Flaggen Ausrichtung angepasst

--- VS Projekt/Underlord/Underlord/Underlord/Logic/Vars_Func.cs
+++ VS Projekt/Underlord/Underlord/Underlord/Logic/Vars_Func.cs
@@ -89,13 +89,9 @@ namespace Underlord.Logic
            NestModels[(int)NestTyp.Beetle].Texture = Content.Load<Texture2D>("Textures//nest_orange_TEXT");
            //NestModels[(int)NestTyp.Entrance].Texture = Content.Load<Texture2D>("Textures//nest_orange_TEXT");
 
-           UpgradeModels.Add(new BasicModel(Content.Load<Model>("Models//nest_HEX_01")));
-           UpgradeModels.Add(new BasicModel(Content.Load<Model>("Models//nest_HEX_01")));
-           UpgradeModels.Add(new BasicModel(Content.Load<Model>("Models//nest_HEX_01")));
-
-           UpgradeModels[(int)UpgradeTyp.Damage].Texture = Content.Load<Texture2D>("Textures//nest_orange_TEXT");
-           UpgradeModels[(int)UpgradeTyp.Life].Texture = Content.Load<Texture2D>("Textures//nest_orange_TEXT");
-           UpgradeModels[(int)UpgradeTyp.Speed].Texture = Content.Load<Texture2D>("Textures//nest_orange_TEXT");
+           UpgradeModels.Add(new BasicModel(Content.Load<Model>("Models//flag_Deg_GEO_01")));
+           UpgradeModels.Add(new BasicModel(Content.Load<Model>("Models//flag_Lve_GEO_01")));
+           UpgradeModels.Add(new BasicModel(Content.Load<Model>("Models//flag_Spd_GEO_01")));
 
            CreatureModels.Add(new AnimationModel(Content.Load<Model>("AnimationModels//ant_GEO_01")));
            CreatureModels.Add(new AnimationModel(Content.Load<Model>("AnimationModels//ant_GEO_01")/*, 0.2f, MathHelper.PiOver2*/));
